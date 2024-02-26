a0bf6ce1cb84c5e6a979507f93f2a344bf42134a
phillipbjj <pjdempsey3@gmail.com>
2024-02-26 12:40:04 -0600

    trying to get tailwind to work :/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './Twom.pro_chatapp/**/*.html',
-    './Twom.pro_chatapp/**/*.js',
-    './Twom.pro_chatapp/**/*.jsx',
-  ],
+  content: [],
   theme: {
     extend: {},
   },
