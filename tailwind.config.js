87a33c7a4e23bf0c6b36e671163922c32bc68c99
phillipbjj <pjdempsey3@gmail.com>
2024-02-26 11:55:12 -0600

    added directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './Twom.pro_chatapp/**/*.html',
+    './Twom.pro_chatapp/**/*.js',
+    './Twom.pro_chatapp/**/*.jsx',
+  ],
   theme: {
     extend: {},
   },
