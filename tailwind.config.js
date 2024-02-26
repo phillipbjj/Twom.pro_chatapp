14405a73b491b1a8acf4d27c5c874dc487cc4c98
phillipbjj <pjdempsey3@gmail.com>
2024-02-26 13:24:13 -0600

    arrrr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
   theme: {
     extend: {},
   },
