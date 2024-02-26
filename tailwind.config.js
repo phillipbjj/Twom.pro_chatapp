ab227c8cd0b72dc953750f451affaf27dfe86493
phillipbjj <pjdempsey3@gmail.com>
2024-02-26 11:34:05 -0600

    renamed output file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.html',
+  './src/**/*.js', './src/**/*.css'],
   theme: {
     extend: {},
   },
