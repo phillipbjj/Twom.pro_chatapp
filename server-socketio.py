2b51657d5cf428b474cc90ca1ddbab07d3b3c9ee
phillipbjj <pjdempsey3@gmail.com>
2024-02-27 09:07:16 -0600

    socket.io file added

---
+++ server-socketio.py
@@ -0,0 +1,25 @@
+import socketio
+
+# Create a Socket.IO server
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+# Define event handlers
+@sio.event
+def connect(sid, environ):
+    print('Client connected:', sid)
+
+@sio.event
+def disconnect(sid):
+    print('Client disconnected:', sid)
+
+@sio.event
+def chat_message(sid, data):
+    sio.emit('chat message', data)
+
+# Run the Socket.IO server
+if __name__ == '__main__':
+    import eventlet
+    import eventlet.wsgi
+
+    eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
