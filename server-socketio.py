27a910df8071f275be0e7ac02b0d7462a3d46b4a
= <=>
2024-03-15 09:45:28 -0500

    updated socket io ip address

--- server-socketio.py
+++ server-socketio.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     import eventlet
     import eventlet.wsgi
 
-    eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
