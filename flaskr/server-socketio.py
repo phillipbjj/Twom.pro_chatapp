d8f87da990d5304a90a204e380892be2ad5eb7d4
phillipbjj <pjdempsey3@gmail.com>
2024-03-19 11:17:29 -0500

    0.0.0.0

--- flaskr/server-socketio.py
+++ flaskr/server-socketio.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     import eventlet
     import eventlet.wsgi
 
-    eventlet.wsgi.server(eventlet.listen(('18.220.33.51', 5000)), app) #'18.220.33.51', 127.0.0.1
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app) #'18.220.33.51', 127.0.0.1
